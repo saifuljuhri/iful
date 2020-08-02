3abfe0b50af21ab67bcd4b223eb61d9d2869802e
saifuljuhri <69101025+saifuljuhri@users.noreply.github.com>
2020-08-02 05:57:46 +0000

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.55.4";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.9170289,107.6098005";
+$header[] = "X-Location: -6.2106668,106.8452398";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
